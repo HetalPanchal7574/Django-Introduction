31c28ee3ff40a987c61f8afc7f9a2c93965db8dd
HetalPanchal7574 <160516704+HetalPanchal7574@users.noreply.github.com>
2024-08-05 22:12:10 +0530

    Initial commit

---
+++ userproject/home/urls.py
@@ -0,0 +1,11 @@
+from turtle import home
+from django.contrib import admin
+from django.urls import path, include
+from home import views
+
+urlpatterns = [
+    path('', views.index, name="home"),
+    path('login', views.loginUser, name="login"),
+    path('logout', views.logoutUser, name="logout"),
+    
+]
